ddaf1edfe595b4fed63b3a0df1d30bbf4146e506
Salah Ashraf <salahashraf924@gmail.com>
2023-02-22 20:10:20 +0200

    Initial commit

---
+++ src/main/java/com/example/gp/GpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gp/GpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
