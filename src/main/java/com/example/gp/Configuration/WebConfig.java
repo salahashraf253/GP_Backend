7f92915e1c801ee31cb446d71b656f0af048818f
Salah Ashraf <salahashraf924@gmail.com>
2023-03-08 15:01:09 +0200

    move WebConfig.java in Configuration package

--- src/main/java/com/example/gp/Configuration/WebConfig.java
+++ src/main/java/com/example/gp/Configuration/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.gp;
+package com.example.gp.Configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
