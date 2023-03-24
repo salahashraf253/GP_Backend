4816b71f3037eac954071dc584460890a9b4fe65
Salah Ashraf <salahashraf924@gmail.com>
2023-03-24 02:16:50 +0200

    document the code

--- src/main/java/com/example/gp/Configuration/WebConfig.java
+++ src/main/java/com/example/gp/Configuration/WebConfig.java
@@ -4,9 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * this class is used for providing configuration for the spring framework application
+ * */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+    *  is used to configure CORS to allow any origin to make GET, POST, PUT, and DELETE requests to the application,
+     *  and to allow the Content-Type and Authorization headers in requests.
+    * */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
