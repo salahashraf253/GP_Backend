85655ec2b74e6e29da5096771618beac1d6c2b8e
Salah Ashraf <salahashraf924@gmail.com>
2023-03-31 21:01:25 +0200

    Make the class extends Recommender Abstract class

--- src/main/java/com/example/gp/Hotel/Recommender/HotelRecommenderService.java
+++ src/main/java/com/example/gp/Hotel/Recommender/HotelRecommenderService.java
@@ -1,25 +1,16 @@
 package com.example.gp.Hotel.Recommender;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import com.example.gp.Recommender;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
 
 @Service
-public class HotelRecommenderService {
-    @Autowired
-    private RestTemplate restTemplate;
+public class HotelRecommenderService extends Recommender {
 
-    public Integer[] getRecommendedHotels(int userId,String city){
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-
-        String url = "http://127.0.0.1:5000/hotel/"+ userId + "/" +city ;
+    @Override
+    public Integer[] recommend(int userId, String city) {
+        String url = baseUrl + "/hotel/" + userId + "/" + city;
         ResponseEntity<Integer[]> response = restTemplate.getForEntity(url, Integer[].class);
         if (response.getStatusCode() == HttpStatus.OK) {
             return response.getBody();
